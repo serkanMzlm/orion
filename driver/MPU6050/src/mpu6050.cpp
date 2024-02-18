c0a901cdccb7085184a31bbb5be85fc45ef172bb
serkan <serkanmazlum306@gmail.com>
2024-02-18 19:46:39 +0300

    The MPU6050 code has been added.

--- driver/MPU6050/src/mpu6050.cpp
+++ driver/MPU6050/src/mpu6050.cpp
@@ -1,7 +1,26 @@
 #include "mpu6050_driver.hpp"
+#include <signal.h>
+
+void handleCtrlC(int signum) {
+    std::cout << "Ctrl+C program terminated" << std::endl;
+    exit(signum);
+}
 
 int main(){
+    MPU6050 mpu6050;
+    for(int i = 0; i < 10; i++) {
+        std::cout << "\n";
+    }
 
+    signal(SIGINT, handleCtrlC);
+
+    while (true) {
+        mpu6050.cleanTerminal();
+        mpu6050.printAcceleration();
+        mpu6050.printAngularVelocity();
+
+        usleep(10000);
+    }
 
     return 0;
 }
